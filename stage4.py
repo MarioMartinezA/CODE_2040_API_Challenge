4d4f3efa4617df055ad5a5e1e9c8493e2e9f0e79
Mario <mariomartinez1@csumb.edu>
2016-09-05 12:31:37 -0700

    Displaying only words to the screen that don’t contain the prefix.

--- stage4.py
+++ stage4.py
@@ -5,9 +5,18 @@ api_token = "87077d8cec8244ce87b5f720efca70af"
 url_prefix = "http://challenge.code2040.org/api/prefix"
 prefix = 'prefix'
 array = 'array'
+container = []
 
 json_data = {'token': api_token}
 
 request = requests.post(url_prefix, json_data)
 
-print request.text
+json = request.json()
+prefix = json[prefix]
+array = json[array]
+
+print "Prefix:", prefix
+
+for element in array:
+    if prefix != element[:len(prefix)]:
+        print element
