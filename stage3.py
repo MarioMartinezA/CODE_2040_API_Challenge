6fc30bb860c23807c2cc354a02a86699f58f16a5
Mario <mariomartinez1@csumb.edu>
2016-09-04 22:36:08 -0700

    A for loop runs through the array and displays values of each element. The counter is incremented each time it runs through each element. Once the element matches the value we are looking for, the for loop stops and displays the index of where the for loop stopped.

--- stage3.py
+++ stage3.py
@@ -3,6 +3,9 @@ import json
 
 api_token = "87077d8cec8244ce87b5f720efca70af"
 url_haystack = "http://challenge.code2040.org/api/haystack"
+index = 0
+value_to_find = 'needle'
+array = 'haystack'
 
 json_data = {"token": api_token}
 
@@ -11,8 +14,13 @@ request = requests.post(url_haystack, json_data)
 #Calling the json decoder.
 json = request.json()
 
+value_to_find = json[value_to_find]
+print value_to_find
 
-print "Needle: " + json['needle']
-
-for element in json['haystack']:
+for element in json[array]:
     print element
+    if element == value_to_find:
+        break
+    index += 1
+
+print index
