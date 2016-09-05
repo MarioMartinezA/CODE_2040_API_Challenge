5b6700674c6d2f04bddc2d5f8f231cfee25f6a0b
Mario <mariomartinez1@csumb.edu>
2016-09-04 22:43:58 -0700

    Just sending the results to the API.

--- stage3.py
+++ stage3.py
@@ -3,6 +3,7 @@ import json
 
 api_token = "87077d8cec8244ce87b5f720efca70af"
 url_haystack = "http://challenge.code2040.org/api/haystack"
+url_validate = "http://challenge.code2040.org/api/haystack/validate"
 index = 0
 value_to_find = 'needle'
 array = 'haystack'
@@ -15,12 +16,13 @@ request = requests.post(url_haystack, json_data)
 json = request.json()
 
 value_to_find = json[value_to_find]
-print value_to_find
 
 for element in json[array]:
-    print element
     if element == value_to_find:
         break
     index += 1
 
-print index
+json_data = {"token": api_token, "needle": index}
+send_data = requests.post(url_validate, json_data)
+
+print send_data.text
