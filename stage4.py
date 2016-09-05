9fd0ffaf3f7e2569777c21457b813175c8b6ceb1
Mario <mariomartinez1@csumb.edu>
2016-09-05 13:12:09 -0700

    Storing all the words that don’t contain the prefix in a list. Before sending the dictionary, I had to encode it. Since I had to decode it, in order to traverse through the given array using a for loop.

--- stage4.py
+++ stage4.py
@@ -3,6 +3,7 @@ import json
 
 api_token = "87077d8cec8244ce87b5f720efca70af"
 url_prefix = "http://challenge.code2040.org/api/prefix"
+url_validate = "http://challenge.code2040.org/api/prefix/validate"
 prefix = 'prefix'
 array = 'array'
 container = []
@@ -11,12 +12,19 @@ json_data = {'token': api_token}
 
 request = requests.post(url_prefix, json_data)
 
+#Decoding the json data
 json = request.json()
 prefix = json[prefix]
 array = json[array]
 
-print "Prefix:", prefix
-
+#Storing all the elements that don't contain the prefix in a list.
 for element in array:
     if prefix != element[:len(prefix)]:
-        print element
+        container.append(element)
+
+json_data = {"token": api_token, "array": container}
+#Before we send the json data back we have to encode it.
+send_data = requests.post(url_validate, json=json_data)
+
+print send_data.text
+
