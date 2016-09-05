ed6fd8c13c180bb2d8d842c575f1cc2893e9f219
Mario <mariomartinez1@csumb.edu>
2016-09-04 22:07:44 -0700

    Sent the reversed string along with token to the API.  I was having issues with sending the json data to the API. To fix this I redefined the json right before sending post to the API.

--- stage2.py
+++ stage2.py
@@ -5,16 +5,20 @@ api_token = "87077d8cec8244ce87b5f720efca70af"
 github = "https://github.com/MarioMartinezA/CODE_2040_API_Challenge"
 url_reverse = "http://challenge.code2040.org/api/reverse"
 url_validate = "http://challenge.code2040.org/api/reverse/validate"
-reverse_string = ""
 
-json_data = {"token": api_token, "string": reverse_string}
+json_data = {"token": api_token}
 
 request = requests.post(url_reverse, json_data)
 
-print request.text
 
 #Reversing the string
 reverse_string = request.text
 reverse_string = reverse_string[::-1]
 
-print reverse_string
+json_data = {"token": api_token, "string": reverse_string}
+send_data = requests.post(url_validate, json_data)
+
+
+print send_data.text
+
+
