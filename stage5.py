dabc579370c8e2495cc4e04fcf837bb68a2c5b02
Mario <mariomartinez1@csumb.edu>
2016-09-05 20:23:48 -0700

    The timestamp now contains the new timestamp, and it’s sent to the API.

--- stage5.py
+++ stage5.py
@@ -1,36 +1,42 @@
 import json
 import requests
 import datetime
+import dateutil.parser as dp
 
 api_token = "87077d8cec8244ce87b5f720efca70af"
 url_dating = "http://challenge.code2040.org/api/dating"
+url_validate = "http://challenge.code2040.org/api/dating/validate"
+
 json_data = {"token": api_token}
 date = "datestamp"
 interval = "interval"
-iso8601_format = "%Y-%m-%dT%H:%M:%S%fZ" 
+
+iso8601_format = "%Y-%m-%dT%H:%M:%SZ"
 
 request = requests.post(url_dating, json_data)
 
+#Grabbing the date and the interval
 json = request.json()
 date = json[date]
 interval = json[interval]
+
+print date, interval
 
 #Stripping the time from the string
 timestamp = datetime.datetime.strptime(date, iso8601_format)
 
 #Adding the seconds to the time
-timestamp = timestamp + datetime.timedelta(seconds=int(interval))
+interval = long(interval)
+timestamp = timestamp + datetime.timedelta(seconds = interval)
+new_time = timestamp.isoformat()
+new_time = str(new_time) + 'Z'
 
-#Converting it to a string
-timestamp = str(timestamp)
+print new_time
 
-#Formatting our string back to it's original format
-timestamp = timestamp.replace(' ', 'T')
-timestamp = timestamp.replace('.', 'Z')
+json_data = {"token": api_token, "datestamp": new_time}
 
-print date
-print timestamp[:len(timestamp) - 6]
+send_data = requests.post(url_validate, json=json_data)
 
-
+print send_data.text
 
 
